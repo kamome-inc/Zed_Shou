14b85a5162c5b0bafc1616cf2c4938744ed09e54
Chayka <58815424+kamome-inc@users.noreply.github.com>
2020-02-07 12:55:33 +0300

    2

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
 from sys import exit
+import modul_zero
 
-
-def gold_room():
-    print("The room are full of gold! How much you will take?")
-
-    choise = input("> ")
-    if '0' in choise or '1' in choise:
-        how_much = int(choise)
-    else:
-        print("Hey, you have to enter a number!")
-
-    if how_much < 50:
-        print("Great! You are not greedy, so you won!")
-        exit(0)
-    else:
-        print("FOOOOOOOOOO")
-        exit(0)
-
-gold_room()
+print(modul_zero.zero())
